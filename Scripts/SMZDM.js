87c9f95cce8db4924a2b8dfeabf52bbc30912c22
XZChen <dr.xzchen@xzchen.pro>
2020-03-04 20:35:24 +0800

    Update SMZDM.js

--- Scripts/SMZDM.js
+++ Scripts/SMZDM.js
@@ -2,6 +2,13 @@ let body = JSON.parse($response.body);
 if (body.data.hasOwnProperty('banner')) {
   delete body.data.banner.big_banner;
   delete body.data.banner.tonglan_banner;
+  delete body.data.banner.calendar_banner;
+}
+if (body.data.hasOwnProperty('big_banner')) {
+  delete body.data.big_banner;
+}
+if (body.data.hasOwnProperty('notice')) {
+  delete body.data.notice;
 }
 body.data.rows.forEach((element, index) => {
   if (element.article_channel_id == -1) {
